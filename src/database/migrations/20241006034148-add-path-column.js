bbdcb94b06d46dc4565b6e7840868b31ab6bf604
heltonjuniodesouza <heltonjuniosouza@gmail.com>
2024-11-27 20:46:59 -0300

    modificaourls

--- src/database/migrations/20241006034148-add-path-column.js
+++ src/database/migrations/20241006034148-add-path-column.js
@@ -4,14 +4,14 @@
 module.exports = {
   async up (queryInterface, Sequelize) {
     
-      await queryInterface.addColumn('categories', 'path',{
+      await queryInterface.addColumn('Categories', 'path',{
         type: Sequelize.STRING
       });
      
   },
 
   async down (queryInterface) {
-     await queryInterface.removeColumn('categories','path');
+     await queryInterface.removeColumn('Categories','path');
      
   }
 };
